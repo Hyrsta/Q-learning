1217c518cc6d2896e8293370391f37d7a3e85408
Hyrsta <leonardo543216@gmail.com>
2025-10-31 22:19:54 +0800

    change envs

--- src/dqn_experiments/envs.py
+++ src/dqn_experiments/envs.py
@@ -36,19 +36,16 @@ def make_env(
     video_trigger: Optional[Callable[[int], bool]] = None,
     video_prefix: str = "rl-video",
 ) -> gym.Env:
-    make_id = f"{ATARI_PREFIX}{env_id}" if env_id in ATARI_ENVS else env_id
-    make_kwargs = {}
-    if env_id in ATARI_ENVS:
-        make_kwargs["frameskip"] = 1
-
-    env = gym.make(make_id, render_mode=render_mode, **make_kwargs)
     
-    env.action_space.seed(seed)
-
     if env_id in ATARI_ENVS:
+        env = gym.make(f"{ATARI_PREFIX}{env_id}", render_mode=render_mode, frameskip=1)
+        env.action_space.seed(seed)
         env = AtariPreprocessing(env, grayscale_obs=True, frame_skip=4, screen_size=84, scale_obs=False)
         env = TransformObservation(env, lambda obs: np.array(obs, dtype=np.float32) / 255.0)
         env = FrameStack(env, frame_stack)
+    else:
+        env = gym.make(env_id, render_mode=render_mode)
+        env.action_space.seed(seed)
 
     if video_dir is not None:
         if render_mode not in {"rgb_array", "rgb_array_list"}:
